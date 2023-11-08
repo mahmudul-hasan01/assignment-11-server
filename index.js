47689468c627d973834fbb8f8c5e6225c0051b49
mahmudul-hasan01 <mahamudul2018@gmail.com>
2023-11-08 11:15:50 +0600

    delete purchase data

--- index.js
+++ index.js
@@ -123,8 +123,12 @@ async function run() {
         console.log(error)
       }
     })
-    
-  
+    app.delete('/PurchaseData/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await PurchaseInfo.deleteOne(query)
+      res.send(result)
+    })
 
 
     // await client.db("admin").command({ ping: 1 });
